84a4c1b92265a8b6f1c530cc08c91e7a9488d185
grindingsugarr <grindingsugarr@gmail.com>
2026-09-07 01:09:16 +0700

    Update database.py

--- database.py
+++ database.py
@@ -4,32 +4,44 @@ from datetime import datetime
 conn = sqlite3.connect("users.db", check_same_thread=False)
 cursor = conn.cursor()
 
+
 cursor.execute("""
 CREATE TABLE IF NOT EXISTS users(
-    user_id INTEGER PRIMARY KEY,
+    chat_id INTEGER,
+    user_id INTEGER,
     username TEXT,
     name TEXT,
     messages INTEGER DEFAULT 0,
-    last_active TEXT
+    last_active TEXT,
+    PRIMARY KEY(chat_id, user_id)
 )
 """)
 
 conn.commit()
 
 
-def update_user(user):
+def update_user(chat_id, user):
     username = user.username or ""
-    name = user.first_name
+    name = user.first_name or "Unknown"
 
     cursor.execute("""
-    INSERT INTO users(user_id, username, name, messages, last_active)
-    VALUES (?, ?, ?, ?, ?)
-    ON CONFLICT(user_id)
+    INSERT INTO users(
+        chat_id,
+        user_id,
+        username,
+        name,
+        messages,
+        last_active
+    )
+    VALUES (?, ?, ?, ?, ?, ?)
+
+    ON CONFLICT(chat_id, user_id)
     DO UPDATE SET
-    messages = messages + 1,
-    last_active = ?
+        messages = messages + 1,
+        last_active = ?
     """,
     (
+        chat_id,
         user.id,
         username,
         name,
@@ -41,10 +53,14 @@ def update_user(user):
     conn.commit()
 
 
-def get_users():
+
+def get_users(chat_id):
+
     cursor.execute("""
     SELECT user_id, username, name, messages
     FROM users
-    """)
+    WHERE chat_id = ?
+    """,
+    (chat_id,))
 
     return cursor.fetchall()
