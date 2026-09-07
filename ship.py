f4bfade6a65a7ed6858a16dd51d8d389a3a97f4f
grindingsugarr <grindingsugarr@gmail.com>
2026-09-07 15:11:13 +0700

    Update ship.py

--- ship.py
+++ ship.py
@@ -1,25 +1,64 @@
 import random
 
 
+def calculate_compatibility(user1, user2):
+
+    id1, username1, name1, messages1 = user1
+    id2, username2, name2, messages2 = user2
+
+    # aktivitas grup
+    activity_score = min(
+        ((messages1 + messages2) / 200) * 40,
+        40
+    )
+
+    # keseimbangan aktivitas
+    interaction_score = random.randint(10, 30)
+
+    # faktor chemistry kecil
+    random_score = random.randint(5, 15)
+
+    total = int(
+        activity_score +
+        interaction_score +
+        random_score
+    )
+
+    return min(max(total, 50), 99)
+
+
+
 def choose_ship(users):
 
     if len(users) < 2:
         return None
 
-    weighted_users = []
+    best_pair = None
+    best_score = 0
 
-    for user in users:
-        user_id, username, name, messages = user
 
-        weight = max(messages, 1)
+    for i in range(len(users)):
 
-        for _ in range(weight):
-            weighted_users.append(user)
+        for j in range(i + 1, len(users)):
 
-    person1 = random.choice(weighted_users)
-    person2 = random.choice(weighted_users)
+            user1 = users[i]
+            user2 = users[j]
 
-    while person1[0] == person2[0]:
-        person2 = random.choice(weighted_users)
 
-    return person1, person2
+            score = calculate_compatibility(
+                user1,
+                user2
+            )
+
+
+            if score > best_score:
+
+                best_score = score
+                best_pair = (
+                    user1,
+                    user2,
+                    score
+                )
+
+
+    return best_pair
